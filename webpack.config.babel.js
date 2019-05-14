225fb5f17f8f5dc9b8b6eb7bdf75559bc7c409f5
Christian Westgaard <cwe@enonic.com>
2019-05-14 13:32:13 +0200

    Handle ecmascript module assets

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,6 +5,7 @@
 import glob from 'glob';
 import path from 'path';
 import CleanWebpackPlugin from 'clean-webpack-plugin';
+import EsmWebpackPlugin from '@purtuga/esm-webpack-plugin';
 /*import jssCamelCase from 'jss-camel-case';
 import jssDefaultUnit from 'jss-default-unit';
 //import jssGlobal from 'jss-global';
@@ -14,7 +15,6 @@ import jssVendorPrefixer from 'jss-vendor-prefixer';*/
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import postcssPresetEnv from 'postcss-preset-env';
 import UglifyJsPlugin from 'uglifyjs-webpack-plugin'; // Supports ECMAScript2015
-
 
 //──────────────────────────────────────────────────────────────────────────────
 // Common
@@ -40,7 +40,7 @@ const stats = {
 	timings: false,
 	version: false
 };
-
+const WEBPACK_CONFIG = [];
 
 //──────────────────────────────────────────────────────────────────────────────
 // Functions
@@ -62,78 +62,74 @@ const SERVER_JS_FILES = glob.sync(`${SRC_DIR}/**/${JS_EXTENSION_GLOB_BRACE}`, {
 	ignore: ALL_JS_ASSETS_FILES
 });
 //console.log(`SERVER_JS_FILES:${toStr(SERVER_JS_FILES)}`);
-if (!SERVER_JS_FILES.length) {
-	console.error('Webpack did not find any files to process!');
-	process.exit();
+
+if (SERVER_JS_FILES.length) {
+	const SERVER_JS_ENTRY = dict(SERVER_JS_FILES.map(k => [
+		k.replace(`${SRC_DIR}/`, '').replace(/\.[^.]*$/, ''), // name
+		`.${k.replace(`${SRC_DIR}`, '')}` // source relative to context
+	]));
+	//console.log(`SERVER_JS_ENTRY:${toStr(SERVER_JS_ENTRY)}`);
+
+	const SERVER_JS_CONFIG = {
+		context,
+		entry: SERVER_JS_ENTRY,
+		externals: [
+			/^\//
+		],
+		devtool: false, // Don't waste time generating sourceMaps
+		mode: MODE,
+		module: {
+			rules: [{
+				test: /\.(es6?|js)$/, // Will need js for node module depenencies
+				use: [{
+					loader: 'babel-loader',
+					options: {
+						babelrc: false, // The .babelrc file should only be used to transpile config files.
+						comments: false,
+						compact: false,
+						minified: false,
+						plugins: [
+							'array-includes',
+							//'import-css-to-jss', // NOTE This will hide the css from MiniCssExtractPlugin!
+							//'optimize-starts-with', https://github.com/xtuc/babel-plugin-optimize-starts-with/issues/1
+							//'transform-prejss',
+							'@babel/plugin-proposal-object-rest-spread',
+							'@babel/plugin-transform-object-assign'
+						],
+						presets: [
+							[
+								'@babel/preset-env',
+								{
+									useBuiltIns: false // false means polyfill not required runtime
+								}
+							]
+						]
+					} // options
+				}]
+			}]
+		}, // module
+		optimization: {
+			minimizer: [
+				new UglifyJsPlugin({
+					parallel: true, // highly recommended
+					sourceMap: false
+				})
+			]
+		},
+		output: {
+			path: outputPath,
+			filename: '[name].js',
+			libraryTarget: 'commonjs'
+		}, // output
+		resolve: {
+			extensions
+		}, // resolve
+		stats
+	};
+	//console.log(`SERVER_JS_CONFIG:${JSON.stringify(SERVER_JS_CONFIG, null, 4)}`);
+	WEBPACK_CONFIG.push(SERVER_JS_CONFIG);
 }
 
-const SERVER_JS_ENTRY = dict(SERVER_JS_FILES.map(k => [
-	k.replace(`${SRC_DIR}/`, '').replace(/\.[^.]*$/, ''), // name
-	`.${k.replace(`${SRC_DIR}`, '')}` // source relative to context
-]));
-//console.log(`SERVER_JS_ENTRY:${toStr(SERVER_JS_ENTRY)}`);
-
-const BABEL_USE = {
-	loader: 'babel-loader',
-	options: {
-		babelrc: false, // The .babelrc file should only be used to transpile config files.
-		comments: false,
-		compact: false,
-		minified: false,
-		plugins: [
-			'array-includes',
-			//'import-css-to-jss', // NOTE This will hide the css from MiniCssExtractPlugin!
-			//'optimize-starts-with', https://github.com/xtuc/babel-plugin-optimize-starts-with/issues/1
-			//'transform-prejss',
-			'@babel/plugin-proposal-object-rest-spread',
-			'@babel/plugin-transform-object-assign'
-		],
-		presets: [
-			[
-				'@babel/preset-env',
-				{
-					useBuiltIns: false // false means polyfill not required runtime
-				}
-			]
-		]
-	} // options
-};
-
-const ES_RULE = {
-	test: /\.(es6?|js)$/, // Will need js for node module depenencies
-	use: [BABEL_USE]
-};
-
-const SERVER_JS_CONFIG = {
-	context,
-	entry: SERVER_JS_ENTRY,
-	externals: [
-		/^\//
-	],
-	devtool: false, // Don't waste time generating sourceMaps
-	mode: MODE,
-	module: {
-		rules: [ES_RULE]
-	}, // module
-	optimization: {
-		minimizer: [
-			new UglifyJsPlugin({
-				parallel: true, // highly recommended
-				sourceMap: false
-			})
-		]
-	},
-	output: {
-		path: outputPath,
-		filename: '[name].js',
-		libraryTarget: 'commonjs'
-	}, // output
-	resolve: {
-		extensions
-	}, // resolve
-	stats
-};
-//console.log(`SERVER_JS_CONFIG:${JSON.stringify(SERVER_JS_CONFIG, null, 4)}`);
 
 //──────────────────────────────────────────────────────────────────────────────
 // Styling
@@ -209,7 +205,34 @@ const STYLE_CONFIG = {
 				loader: 'svg-url-loader',
 				options: {}
 			}
-		}, ES_RULE]
+		}, {
+			test: /\.(es6?|js)$/, // Will need js for node module depenencies
+			use: [{
+				loader: 'babel-loader',
+				options: {
+					babelrc: false, // The .babelrc file should only be used to transpile config files.
+					comments: false,
+					compact: false,
+					minified: false,
+					plugins: [
+						'array-includes',
+						//'import-css-to-jss', // NOTE This will hide the css from MiniCssExtractPlugin!
+						//'optimize-starts-with', https://github.com/xtuc/babel-plugin-optimize-starts-with/issues/1
+						//'transform-prejss',
+						'@babel/plugin-proposal-object-rest-spread',
+						'@babel/plugin-transform-object-assign'
+					],
+					presets: [
+						[
+							'@babel/preset-env',
+							{
+								useBuiltIns: false // false means polyfill not required runtime
+							}
+						]
+					]
+				} // options
+			}]
+		}]
 	}, // module
 	output: {
 		path: path.join(__dirname, '.build')
@@ -234,14 +257,87 @@ const STYLE_CONFIG = {
 };
 //console.log(`STYLE_CONFIG:${JSON.stringify(STYLE_CONFIG, null, 4)}`);
 
+WEBPACK_CONFIG.push(STYLE_CONFIG);
+
+
+//──────────────────────────────────────────────────────────────────────────────
+const ESM_ASSETS_GLOB = `${SRC_DIR}/${ASSETS_PATH_GLOB_BRACE}/**/*.{jsx,mjs}`;
+const ESM_ASSETS_FILES = glob.sync(ESM_ASSETS_GLOB);
+//console.log(`ESM_ASSETS_FILES:${toStr(ESM_ASSETS_FILES)}`);
+
+if (ESM_ASSETS_FILES.length) {
+	const ASSETS_ESM_ENTRY = dict(ESM_ASSETS_FILES.map(k => [
+		k.replace(`${SRC_DIR}/assets/`, '').replace(/\.[^.]*$/, ''), // name
+		`.${k.replace(`${SRC_DIR}/assets`, '')}` // source relative to context
+	]));
+	//console.log(`ASSETS_ESM_ENTRY:${toStr(ASSETS_ESM_ENTRY)}`);
+
+	const ASSETS_ESM_CONFIG = {
+		context: path.resolve(__dirname, SRC_DIR, 'assets'),
+		entry: ASSETS_ESM_ENTRY,
+		mode: MODE,
+		module: {
+			rules: [{
+				test: /\.(es6?|m?jsx?)$/, // Will need js for node module depenencies
+				use: [{
+					loader: 'babel-loader',
+					options: {
+						babelrc: false, // The .babelrc file should only be used to transpile config files.
+						comments: false,
+						compact: false,
+						minified: false,
+						plugins: [
+							//'@babel/plugin-proposal-class-properties',
+							'@babel/plugin-proposal-object-rest-spread',
+							//'@babel/plugin-syntax-dynamic-import',
+							'@babel/plugin-transform-object-assign',
+							/*['@babel/plugin-transform-runtime', { // This destroys esm?
+								regenerator: true
+							}],*/
+							'array-includes'
+						],
+						presets: [
+							[
+								'@babel/preset-env',
+								{
+									useBuiltIns: false // false means polyfill not required runtime
+								}
+							],
+							'@babel/preset-react'
+						]
+					} // options
+				}]
+			}]
+		}, // module
+		output: {
+			path: path.join(__dirname, DST_DIR, 'assets'),
+			filename: '[name].esm.js',
+			library: 'LIB',
+			libraryTarget: 'var'
+		},
+		plugins: [
+			new EsmWebpackPlugin() // exports doesn't exist in Browser
+		],
+		resolve: {
+			extensions: [
+				'.es',
+				'.es6',
+				'.mjs',
+				'.jsx',
+				'.js',
+				'.json'
+			]
+		}, // resolve
+		stats
+	};
+	//console.log(`ASSETS_ESM_CONFIG:${toStr(ASSETS_ESM_CONFIG)}`);
+	WEBPACK_CONFIG.push(ASSETS_ESM_CONFIG);
+}
+
+
 //──────────────────────────────────────────────────────────────────────────────
 // Exports
 //──────────────────────────────────────────────────────────────────────────────
-const WEBPACK_CONFIG = [
-	SERVER_JS_CONFIG,
-	STYLE_CONFIG
-];
-
 //console.log(`WEBPACK_CONFIG:${JSON.stringify(WEBPACK_CONFIG, null, 4)}`);
 //process.exit();
 
