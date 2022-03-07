f1f7110477dd7f52d06c0cb0f8799c1a52b21354
Christian Westgaard <comlock@gmail.com>
2022-03-07 12:57:43 +0100

    test inQuery, like, pathMatch and range

--- src/main/resources/testQueryDsl.ts
+++ src/main/resources/testQueryDsl.ts
@@ -20,6 +20,10 @@ import {
 //@ts-ignore
 import {connect} from '/lib/xp/node';
 
+const inQuery = storage.query.dsl.inQuery;
+const like = storage.query.dsl.like;
+const pathMatch = storage.query.dsl.pathMatch;
+const range = storage.query.dsl.range;
 const term = storage.query.dsl.term;
 
 
@@ -63,18 +67,52 @@ export function testQueryDSL() {
 
 				const nodeA = connection.create({
 					_name: 'a',
-					myString: 'a'
+					myBoolean: true,
+					myNumber: 3,
+					myString: 'hello'
 				});
 				log.info('nodeA:%s', toStr(nodeA));
 
 				const termQueryParams = {
 					count: -1,
 					//query: term('_name', 'a')
-					query: term('myString', 'a')
+					query: term('myString', 'hello')
 				}
 				log.info('termQueryParams:%s', toStr(termQueryParams));
 				const termQueryRes = connection.query(termQueryParams);
 				log.info('termQueryRes:%s', toStr(termQueryRes));
+
+				const inQueryParams = {
+					count: -1,
+					query: inQuery('myString', ['hello'])
+				}
+				log.info('inQueryParams:%s', toStr(inQueryParams));
+				const inQueryRes = connection.query(inQueryParams);
+				log.info('inQueryRes:%s', toStr(inQueryRes));
+
+				const likeQueryParams = {
+					count: -1,
+					query: like('myString', '*ell*')
+				}
+				log.info('likeQueryParams:%s', toStr(likeQueryParams));
+				const likeQueryRes = connection.query(likeQueryParams);
+				log.info('likeQueryRes:%s', toStr(likeQueryRes));
+
+				const rangeQueryParams = {
+					count: -1,
+					query: range('myString', {gt: 1})
+				}
+				log.info('rangeQueryParams:%s', toStr(rangeQueryParams));
+				const rangeQueryRes = connection.query(rangeQueryParams);
+				log.info('rangeQueryRes:%s', toStr(rangeQueryRes));
+
+				const pathMatchQueryParams = {
+					count: -1,
+					query: pathMatch('_path', '/a')
+				}
+				log.info('pathMatchQueryParams:%s', toStr(pathMatchQueryParams));
+				const pathMatchQueryRes = connection.query(pathMatchQueryParams);
+				log.info('pathMatchQueryRes:%s', toStr(pathMatchQueryRes));
 			} // func
 		}); // executeFunction
 	}); // runInContext
